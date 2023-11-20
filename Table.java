b7671fff0dbe4db9e59ccfea977e4293d44bdfdc
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-11-20 19:23:59 +0500

    Limit Added

--- Table.java
+++ Table.java
@@ -4,17 +4,15 @@ public class Table {
     public static void main(String[] args) {
         Scanner value = new Scanner(System.in);
         System.out.print("Enter number to print the table : ");
+        int number = value.nextInt();
+        System.out.print("Enter number of limit to print the table : ");
+        int limit = value.nextInt();
 
-        if (value.hasNextInt()) {
-            int number = value.nextInt();
-            for (int i = 1; i <= 10; i++) {
-                int result = i * number;
-                System.out.println(number + " * " + i + " = " + result);
-            }
-
-        } else {
-            System.out.println("Please provide int value.");
+        for (int i = 1; i <= limit; i++) {
+            int result = i * number;
+            System.out.println(number + " * " + i + " = " + result);
         }
+
         value.close();
     }
 
