4752dde0b46889c94d8da8e867e4efb7068a4fce
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2023-11-30 13:56:07 +0500

    Added

--- Flag.java
+++ Flag.java
@@ -1,20 +1,20 @@
 public class Flag {
     public static void main(String[] args) {
-        for(int i=1; i<=11; i++){
-            for(int j=1; j<=15; j++){
-                if(i==1||i==7||j==1){
+        for (int i = 1; i <= 11; i++) {
+            for (int j = 1; j <= 15; j++) {
+                if (i == 1 || i == 7 || j == 1) {
                     System.out.print("*");
-                }
-                else if((i==2&&j==3)||(i==2&&j==15)||(i==3&&j==3)||(i==3&&j==15)||(i==4&&j==3)||(i==2&&j==8)||(i==4&&j==15)||(i==4&&j==6)||(i==5&&j==3)||(i==5&&j==15)||(i==6&&j==3)||(i==6&&j==15)||(i==2&&j==6)||(i==2&&j==7)||(i==3&&j==6)||(i==4&&j==7)||(i==4&&j==8)){
+                } else if ((i == 2 && j == 3) || (i == 2 && j == 15) || (i == 3 && j == 3) || (i == 3 && j == 15)
+                        || (i == 4 && j == 3) || (i == 4 && j == 15) || (i == 2 && j == 8) || (i == 4 && j == 6)
+                        || (i == 5 && j == 7) || (i == 6 && j == 8) || (i == 4 && j == 10)
+                        || (i == 5 && j == 3) || (i == 5 && j == 15) || (i == 3 && j == 7) || (i == 6 && j == 3)
+                        || (i == 6 && j == 15)) {
                     System.out.print("*");
-                }
-                else if(i==3&&j==8){
-                    System.out.print("@");
-                }
-                else{
+                } else {
                     System.out.print(" ");
                 }
-            }System.out.println();
+            }
+            System.out.println();
         }
     }
 }
