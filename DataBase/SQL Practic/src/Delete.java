e3c019af3aa58d7d907eddc3acb7d2a5092d3c72
MuhammadAliyan10 <135156764+MuhammadAliyan10@users.noreply.github.com>
2024-01-23 12:15:14 +0500

    Added & Updated

--- DataBase/SQL Practic/src/Delete.java
+++ DataBase/SQL Practic/src/Delete.java
@@ -3,7 +3,7 @@ import java.io.InputStream;
 import java.sql.*;
 import java.util.Properties;
 
-public class Books {
+public class Delete {
     public static void main(String[] args) throws Exception {
         Properties properties = new Properties();
         try (InputStream input = new FileInputStream(
@@ -11,23 +11,25 @@ public class Books {
             properties.load(input);
         }
 
-        String url = properties.getProperty("db.url1");
+        String url = properties.getProperty("db.url");
         String username = properties.getProperty("db.username");
         String password = properties.getProperty("db.password");
+
         try (
                 Connection con = DriverManager.getConnection(url, username, password);
                 Statement stmt = con.createStatement()) {
 
-            String strUpdate = "update books set price = 44.44, qty = 44 where id = 1005";
-            stmt.executeUpdate(strUpdate);
-            String strSelect = "select title, price, qty from books";
+            String strSelect = "select name, rollNumber, Section from information";
+
+            String delete = "delete from information where id = 7";
+            stmt.executeUpdate(delete);
             ResultSet rset = stmt.executeQuery(strSelect);
 
             while (rset.next()) {
-                String title = rset.getString("title");
-                double price = rset.getDouble("price");
-                int qty = rset.getInt("qty");
-                System.out.println(title + ", " + price + ", " + qty);
+                String name = rset.getString("name");
+                String rollNumber = rset.getString("rollNumber");
+                String Section = rset.getString("Section");
+                System.out.println(name + ", " + rollNumber + ", " + Section);
 
             }
 
